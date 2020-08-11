c92e7d35135bd1b71a83e63694a6295034299ee2
LuoTianyi712 <12284778415@qq.com>
2020-08-11 13:16:23 +0800

    修改MainActivity,实现计数器功能

--- app/src/main/java/com/neusoft/liufeiyu/carassistantapptest/MainActivity.java
+++ app/src/main/java/com/neusoft/liufeiyu/carassistantapptest/MainActivity.java
@@ -3,12 +3,27 @@ package com.neusoft.liufeiyu.carassistantapptest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int num =0;
+    private TextView textView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        textView = findViewById(R.id.textView_number);
+
+        findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num++;
+                textView.setText(String.valueOf(num));
+            }
+        });
     }
 }
